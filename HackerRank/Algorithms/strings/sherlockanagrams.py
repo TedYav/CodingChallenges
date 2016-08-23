fe1f9b8bc7cb21c40c359a6d542fe312027e1909
TeomanDavid <davidteoman@gmail.com>
2016-08-23 13:19:53 -0700

    minor fixes to binary tree data store--still needs some work

--- HackerRank/Algorithms/strings/sherlockanagrams.py
+++ HackerRank/Algorithms/strings/sherlockanagrams.py
@@ -11,19 +11,19 @@
 
 from collections import Counter
 
-class x
+# class x
 
 class Node:
-	def __init(self, val,  parent=None):
+	def __init__(self, val,  parent=None):
 		self.parent = parent
 		self.initVal(val)
 
 	def __str__(self):
 		s = "";
-		if(self.left.val):
+		if(self.left):
 			s += str(self.left)
-		s += self.val
-		if(self.right.val):
+		s += self.val if self.val else ""
+		if(self.right):
 			s += str(self.right)
 		return s
 
@@ -47,9 +47,9 @@ class Node:
 	def pivot(self, target):
 		self.val = target.val
 		if(target.left.val):
-			target.leftPivot()
+			target.pivot(target.left)
 		elif(target.right.val):
-			target.rightPivot()
+			target.pivot(target.right)
 		else:
 			target.initVal(None)
 
@@ -61,11 +61,10 @@ class Node:
 						self.pivot(self.left)
 					else:
 						self.pivot(self.right)
-				elif(self.parent):
-					self.initVal(None)
 				else:
+					self.initVal(None)
 					# in case we deleted THE WHOLE TREE!
-					return False
+					return True if self.parent else False
 
 			elif(val < self.val):
 				self.left.delVal(val)
@@ -83,13 +82,14 @@ def hs(count):
 
 t = int(input().strip())
 for z in range(t):
-	s = input().strip()
-	c = Counter(s)
-	print(c)
-	c['z'] = 5
-	print(c)
-	print(hs(c))
-	count = Counter(hs(c))
-	print(count)
-	count.update([hs(c))
-	print(count)
+	s = list(input().strip())
+	print(s)
+	test = Node(s[0])
+	print(test)
+	for i in range(1,len(s)):
+		test.addVal(s[i])
+		print(test)
+	for i in range(len(s)-1,-1,-1):
+		result = test.delVal(s[i])
+		print(test)
+		print(result)
