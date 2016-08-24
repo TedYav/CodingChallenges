c66163855dfe51a9703270a279ffdfbbf30fa4eb
TeomanDavid <davidteoman@gmail.com>
2016-08-24 01:50:31 -0700

    btree struct working but too slow

--- HackerRank/Algorithms/strings/sherlockanagrams.py
+++ HackerRank/Algorithms/strings/sherlockanagrams.py
@@ -12,9 +12,38 @@
 from collections import Counter
 
 # class x
+def fact(n):
+	if(n<=1):
+		return 1
+	else:
+		return n * fact(n-1)
 
+# not super efficient, but ok for this
+def nCr(n,r):
+	if r == 0: return 1
+	return fact(n)//(fact(r) * fact(n-r))
+
+
+# container to avoid the root deletion issue
+class bTree():
+	def __init__(self):
+		self.root = Node()
+
+	def __str__(self):
+		return str(self.root)
+
+	def add(self, val):
+		self.root.addVal(val)
+
+	def remove(self, val):
+		self.root = self.root.delVal(val)
+
+# TODO:
+# refactor this monstrosity
+# make it simpler
+# use counter rather than list to avoid popping issue
 class Node:
-	def __init__(self, val,  parent=None):
+	def __init__(self, val=None,  parent=None):
 		self.parent = parent
 		self.initVal(val)
 
@@ -48,36 +77,53 @@ class Node:
 		else:
 			self.initVal(val)
 
-	def pivot(self, target):
-		self.val = target.val
-		if(target.left.val):
-			target.pivot(target.left)
-		elif(target.right.val):
-			target.pivot(target.right)
-		else:
-			target.initVal(None)
-
-	#teststring: askdjfhfhdjsk
+	def findMin(self, cmin=None):
+		if(not cmin or cmin > self.val[0]):
+			cmin = self.val[0]
+		return cmin if not self.left.val else self.left.findMin(cmin)
 
 	def delVal(self, val):
 		if(self.val):
+			# print("DELETING: ", val, " FROM ", self.val, self.left.val, self.right.val)
 			if(val == self.val[0]):
-				# print("FOUND ", val, " DELETING: ", str(self))
-				self.val.pop()
-				if(len(self.val) == 0):
+				if(len(self.val) > 1):
+					self.val.pop()
+				elif(len(self.val) == 1):
 					if(self.left.val or self.right.val):
-						# print("LEFT: ", str(self.left))
-						# print("LEFT VAL:", self.left.val)
-						# print("RIGHT: ", str(self.right))
-						# print("RIGHT VAL:", self.right.val)
-						if(self.left.val):
-							self.pivot(self.left)
+						if(self.left.val and self.right.val):
+							# print("TWO CHILDREN: ", self.left.val[0], self.right.val[0])
+							m = self.right.findMin()
+							# print("MIN: ", m)
+							self.right.delVal(m)
+							self.val = [m]
+						elif(self.left.val):
+							if(self.parent):
+								if(self.parent.val[0] > self.val[0]):
+									self.parent.left = self.left
+								else:
+									self.parent.right = self.left
+								self.left.parent = self.parent
+							else:								
+								self.left.parent = None
+								return self.left
 						else:
-							self.pivot(self.right)
+							if(self.parent):
+								# print("PARENT VAL", self.parent.val, "MY VAL", self.val)
+								# t = self.parent
+								# while(t):
+								# 	# print(t.val, t.left, t.right)
+								# 	t = t.parent
+								if(self.parent.val[0] < self.val[0]):
+									self.parent.right = self.right
+								else:
+									self.parent.left = self.right
+								self.right.parent = self.parent
+							else:
+								self.right.parent = None
+								return self.right
 					else:
+						# print("CLEARING ", self.val)
 						self.initVal(None)
-						# in case we deleted THE WHOLE TREE!
-						return True if self.parent else False
 
 			elif(val < self.val[0]):
 				self.left.delVal(val)
@@ -86,22 +132,44 @@ class Node:
 				self.right.delVal(val)
 
 		# otherwise return true, regardless of whether we deleted anything
-		return True
-
-# roll own class with binary tree to speed this up
-# if it's too slow
-def hs(count):
-	return "".join(sorted(c.elements()))
+		# print("RETURNING ", self)
+		return self
 
 t = int(input().strip())
 for z in range(t):
 	s = list(input().strip())
-	print(s)
-	test = Node(s[0])
-	print(test)
+
+	count = Counter()
+
+	# iterate ofer string sizes
+	test = bTree()
+	idx = 0
+	direction = 1
 	for i in range(1,len(s)):
-		test.addVal(s[i])
-		print(test)
-	for i in range(len(s)-1,-1,-1):
-		result = test.delVal(s[i])
-		print(test)
+		# print("LENGTH: ", i)
+
+		test.add(s[idx + (i*direction) - direction])
+		count.update({str(test): 1})
+		# print(test)
+
+
+		for j in range(len(s)-i):
+			idx = idx + direction
+			test.remove(s[idx - direction])
+			test.add(s[idx + (i*direction) - direction])
+			# print("ADDING: ", s[idx + (i*direction) - direction], idx + (i*direction) - direction, " DELETING: ", s[idx-direction], idx-direction)
+			count.update({str(test): 1})
+			# print(str(test))
+			#pvmupwjjjf
+			# print("REMOVING: ", s[idx-direction])
+			# print(idx + (i*direction) - direction)
+			# print("TEST: ", str(test))
+			# print("COUNT: ", str(count))
+		direction *= -1
+		idx = 0 if direction == 1 else len(s) - 1
+
+	# print(count)
+	ways = [nCr(count[e],2) for e in set(count.elements()) if count[e] > 1]
+	# print(ways)
+	num = sum(ways)
+	print(num)
