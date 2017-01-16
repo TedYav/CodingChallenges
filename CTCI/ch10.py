b8ba1aebacd87d9b0ae511fdbdd891d65ee0179b
TeomanDavid <davidteoman@gmail.com>
2017-01-16 18:44:47 -0500

    Sorting and searching chapter in CTCI

---
+++ CTCI/ch10.py
@@ -0,0 +1,294 @@
+# iterative merge sort using stack :)
+def mergesort(a, cmp=lambda x,y: x<y):
+	start = 0
+	end = len(a)
+	stack = [(start,end)]
+	while stack:
+		indices = stack.pop()
+		# sort operation
+		if len(indices) == 2:
+			start,end = indices
+			middle = (end + start)//2
+			if end - start > 1:
+				stack.append((start, middle, end))
+				stack.append((start, middle))
+				stack.append((middle, end))
+		# merge operation
+		else:
+			start,middle,end = indices
+			temp = []
+			i,j = start,middle
+			while i < middle or j < end:
+				if i < middle and j < end:
+					if cmp(a[i],a[j]):
+						temp.append(a[i])
+						i += 1
+					else:
+						temp.append(a[j])
+						j += 1
+				elif i < middle:
+					temp.append(a[i])
+					i += 1
+				else:
+					temp.append(a[j])
+					j += 1
+			a[start:end] = temp
+	print(a)
+	return a
+
+# stack based randomized quick sort in place
+import random
+def quicksort(a):
+	stack = [(0,len(a)-1)]
+	def swap(i,j): a[i],a[j] = a[j],a[i]
+	while stack:
+		start, end = stack.pop()
+		if end-start > 0:
+			pivot_index = random.randint(start,end)
+			swap(pivot_index, end)
+			pivot = a[end]
+			i,j = start,end-1
+			while i<=j:
+				while a[i] <= pivot and i < end: i += 1
+				while a[j] > pivot and j >= i: j -= 1
+				if i < j:
+					swap(i,j)
+					i += 1
+					j -= 1
+			swap(end,i)
+			stack.append((start,i-1))
+			stack.append((i+1, end))
+	print(a)
+	return a
+
+from functools import reduce
+def radix_sort(arr):
+	place = 1
+	arr_min = min(arr)
+	if arr_min < 0:
+		arr = [i + abs(arr_min) for i in arr]
+	arr_max = max(arr)
+	while 10**(place-1) <= arr_max:
+		buckets = [[] for i in range(10)]
+		for num in arr:
+			digit = get_digit(num, place)
+			buckets[digit].append(num)
+		arr = reduce(lambda x,y: x + y, buckets)
+		place += 1
+	if arr_min < 0:
+		arr = [i - abs(arr_min) for i in arr]
+	return arr
+
+def high_speed_bucket_sort(arr, domain):
+	domain = sorted(domain)
+	buckets = [[] for i in domain]
+	for element in arr: buckets[domain.index(element)].append(element)
+	return reduce(lambda x,y: x+y, buckets)
+
+
+def get_digit(num, place):
+	if place > 1:
+		digit = ((num % (10**place)) - (num % (10**(place-1))))//(10**(place-1))
+	else:
+		digit = num % 10
+	return digit
+
+def quicksort_alternate(arr, left=None, right=None):
+	if left is None and right is None:
+		left,right = 0,len(arr) - 1
+	if right - left > 0:
+		index = partition(arr, left, right)
+		quicksort_alternate(arr, left, index-1)
+		quicksort_alternate(arr, index + 1, right)
+		return arr
+
+def partition(arr, left, right):
+	def swap(i,j): arr[i],arr[j] = arr[j],arr[i]
+	if right-left > 0:
+		pivot = random.randint(left,right)
+		swap(pivot, right)
+		pivot = arr[right]
+		i,j = left,right-1
+		while i<=j:
+			while arr[i] <= pivot and i < right: i += 1 
+			while arr[j] > pivot and j >= i: j -= 1
+			if i < j:
+				swap(i,j)
+				i += 1
+				j -= 1
+		swap(right,i)
+		return i
+
+
+def book_quicksort(arr, left=None, right=None):
+	if left is None and right is None:
+		left, right = 0, len(arr)-1
+
+	index = book_partition(arr, left, right)
+	if left < index - 1:
+		book_quicksort(arr, left, index-1)
+	if index < right:
+		book_quicksort(arr, index, right)
+	return arr
+
+def book_partition(arr, left, right):
+	pivot = arr[(left + right)//2]
+	# pivot = random.randint(left, right) CRASHES -- book sort algorithm does NOT WORK FOR RANDOM PIVOT
+	def swap(i,j): arr[i],arr[j] = arr[j],arr[i]
+	while left <= right:
+		while arr[left] < pivot: left += 1
+		while arr[right] > pivot: right -= 1
+		if left <= right:
+			swap(left, right)
+			left += 1
+			right -= 1
+	return left
+
+# identifies the first element in arr such that arr[result] <= target and arr[result+1] > target or result+1 is not in the array
+def bin_search(arr,target):
+	stack = [(0, len(arr))]
+	while stack:
+		low,high = stack.pop()
+		if high-low <= 1:
+			return low
+		guess = (low + high)//2
+		if arr[guess] <= target:
+			low = guess
+		elif arr[guess] > target:
+			high = guess
+		stack.append((low, high))
+
+def test_sort(method, iterations=10, length=1000):
+	for _ in range(iterations):
+		a = [random.randint(-10*length,10*length) for i in range(length)]
+		a = method(a)
+		for i in range(len(a)-1):
+			assert a[i] <= a[i+1], "ERROR %d not less than %d " % (a[i], a[i+1])
+
+def test_quicksort():
+	test_sort(quicksort)
+
+
+def test_booksort():
+	test_sort(book_quicksort)
+
+def test_radix_sort():
+	test_sort(radix_sort)
+
+def test_merge_sort():
+	test_sort(mergesort)
+
+def test_bin_search():
+	for _ in range(10):
+		arr = sorted([random.randint(-1000,1000) for i in range(1000)])
+		target = random.randint(-1200,1200)
+		result = bin_search(arr, target)
+		print(target)
+		if target >= arr[-1]:
+			assert result == len(arr) - 1, str(arr[result:result+2])
+		elif target < arr[0]:
+			assert result == 0, str(arr[result:result+2])
+		else:
+			assert arr[result] <= target and arr[result+1] > target, str(arr[result-1:result+2])
+
+
+#### PROBLEMS #####
+#### PROBLEMS #####
+#### PROBLEMS #####
+#### PROBLEMS #####
+
+def sorted_merge(a,b):
+	if not b: return a if a else []
+	head = len(a) - 1
+	i,j = head - len(b), len(b) - 1
+	while j >= 0:
+		if i >= 0 and a[i] > b[j]:
+			a[head] = a[i]
+			i -= 1
+		else:
+			a[head] = b[j]
+			j -= 1
+		head -= 1
+	return a
+
+def test_sorted_merge(len_a = 50, len_b = 10):
+	a = sorted([random.randint(0,100) for i in range(len_a)])
+	a.extend([None] * len_b)
+	b = sorted([random.randint(0,150) for i in range(len_b)])
+	a = sorted_merge(a,b)
+	for i in range(len(a) - 1):
+		assert a[i] <= a[i+1]
+	print(a)
+
+from functools import reduce
+def group_anagrams(strings):
+	if not strings:
+		return []
+	else:
+		charmap = generate_charmap()
+		anagrams = {}
+		for word in strings:
+			word_hash = anagram_hash(word, charmap)
+			if word_hash in anagrams:
+				anagrams[word_hash].append(word)
+			else:
+				anagrams[word_hash] = [word]
+		return reduce(lambda x,y: x+y, [anagrams[word_hash] for word_hash in anagrams])
+
+def generate_charmap():
+	primes = generate_primes(26)
+	return {chr(i):primes[i - ord('a')] for i in range(ord('a'),ord('z')+1)}
+
+def anagram_hash(word, charmap=generate_charmap()):
+	return sum(map(lambda c: charmap[c],word))
+
+import math
+def generate_primes(n):
+	nums = list(range(1,n*10, 2))
+	current_prime = 3
+	highest_prime = int(math.sqrt(n*10))
+	while current_prime < highest_prime:
+		index = (current_prime*3)//2
+		while index < len(nums):
+			nums[index] = 0
+			index += current_prime
+		index = (current_prime//2) + 1
+		while index < len(nums):
+			if nums[index] != 0:
+				current_prime = nums[index]
+				break
+			else:
+				index += 1
+	nums[0] = 2
+	return list(filter(lambda x: x > 0, nums))[:n]
+
+def test_group_anagrams(num_anagrams=10, max_length=10):
+	strings = []
+	for _ in range(num_anagrams):
+		word = ''.join([chr(random.randint(ord('a'), ord('z'))) for c in range(random.randint(2, max_length))])
+		print(word)
+		strings.append(word)
+		for i in range(random.randint(0,max_length//2)):
+			strings.append(''.join(shuffle(word)))
+	strings = shuffle(strings)
+	strings = group_anagrams(strings)
+	print(strings)
+	previous_anagrams = []
+	current_hash = 0
+	charmap = generate_charmap()
+	for word in strings:
+		if current_hash != anagram_hash(word, charmap):
+			previous_anagrams.append(current_hash)
+			current_hash = anagram_hash(word, charmap)
+			assert current_hash not in previous_anagrams, "ERROR! Words not properly grouped"
+
+def swap(arr,i,j):
+	arr[i],arr[j] = arr[j],arr[i]
+
+def shuffle(iterable):
+	output = []
+	for i in iterable:
+		output.append(i)
+		if len(output) > 1:
+			swap(output,len(output)-1,random.randint(0,len(output)-1))
+	return output
